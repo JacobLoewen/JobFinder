198dba3143d8ddd150f25cdf9458302f17f4a778
Jacob Loewen <jakeploewen@gmail.com>
2024-12-09 10:42:00 -0800

    Beta Working

--- app.py
+++ app.py
@@ -1,23 +1,116 @@
+import json
+import pymysql
 from flask import Flask, redirect, url_for, request, render_template
 
+import sys
+print("Version", sys.path)
+
+import cryptography
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('indextest.html')  # This will render the index.html file
-
-@app.route('/success/<name>')
-def success(name):
-    return 'Welcome %s' % name
-
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/login')
 def login():
-    if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success', name=user))
-    else:
-        user = request.args.get('nm')
-        return redirect(url_for('success', name=user))
+    return render_template('login.html')  # This will render the login.html file
+
+@app.route('/jobs')
+def jobs():
+
+    return render_template('indexofficial.html')
+
+    # db = pymysql.connect(
+    #     host = 'localhost',
+    #     user = 'JAKEY',
+    #     password = '234',
+    #     db = 'jobsearch',
+    #     cursorclass = pymysql.cursors.DictCursor
+    # )
+
+    # with db.cursor() as cursor:
+    #     # cursor.execute('SELECT * FROM jobs WHERE job_type = \'business_intelligence_developer\';') #This is the SQL statement
+    #     cursor.execute(
+    #         '''SELECT j.job_id, j.job_name, j.company_name, j.location, JSON_ARRAYAGG(k.skill_name) AS skill_name
+    #         FROM jobs j JOIN jobskills s ON j.job_id = s.job_id 
+    #         JOIN skills k ON s.skill_id = k.skill_id
+    #         GROUP BY job_id;'''
+    #     )
+
+    #     jobInfo = cursor.fetchall()
+
+    #     for job in jobInfo:
+    #         job['skill_name'] = json.loads(job['skill_name'])
+    #         # job['skill_name'][1:-2].split(", ")
+
+    #     print(jobInfo)
+
+    #     return render_template("indexofficial.html", job_list=jobInfo)
+
+
+
+
+        # jobStuff = []
+        # for i in result:
+        #     job_details = {
+        #         "jobTitle": str(i['job_name']),
+        #         "company": str(i['company_name']),
+        #         "location": str(i['location']),
+        #         "skills": []
+        #     }
+
+        #     # Fetch jobs for the job id using parameterized queries to prevent SQL injection
+        #     cursor.execute('''
+        #         SELECT s.skill_name 
+        #         FROM jobs i
+        #         JOIN jobskills j ON i.job_id = j.job_id
+        #         JOIN skills s ON j.skill_id = s.skill_id
+        #         WHERE i.job_id = %s;
+        #     ''', (i['job_id'],))  # Use tuple to pass parameters safely
+
+        #     skills = cursor.fetchall()
+        #     for j in skills:
+        #         job_details['skills'].append(str(j['skill_name']))
+            
+        #     # Add the job details to the jobStuff list
+        #     jobStuff.append(job_details)
+
+            # Return the result as a formatted JSON-like string (with proper indentation)
+    # return f"Welcome {jobStuff}"
+        # return 'Welcome %s' % name
+
+@app.route('/login', methods=['POST'])
+def loginPost():
+    # user = request.form['nm']
+    return redirect(url_for('jobs'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/getJobs', methods=['POST'])
+def getJobs():
+    db = pymysql.connect(
+        host = 'localhost',
+        user = 'JAKEY',
+        password = '234',
+        db = 'jobsearch',
+        cursorclass = pymysql.cursors.DictCursor
+    )
+
+    with db.cursor() as cursor:
+        # cursor.execute('SELECT * FROM jobs WHERE job_type = \'business_intelligence_developer\';') #This is the SQL statement
+        cursor.execute(
+            '''SELECT j.job_id, j.job_name, j.job_type, j.company_name, j.location, JSON_ARRAYAGG(k.skill_name) AS skill_name
+            FROM jobs j JOIN jobskills s ON j.job_id = s.job_id 
+            JOIN skills k ON s.skill_id = k.skill_id
+            GROUP BY job_id;'''
+        )
+
+        jobInfo = cursor.fetchall()
+
+        for job in jobInfo:
+            job['skill_name'] = json.loads(job['skill_name'])
+            # job['skill_name'][1:-2].split(", ")
+
+        print(jobInfo)
+
+        return jobInfo
+    
--- main.py
+++ main.py
@@ -11,19 +11,26 @@ db = pymysql.connect(
 
 with db.cursor() as cursor:
     # cursor.execute('SELECT * FROM jobs WHERE job_type = \'business_intelligence_developer\';') #This is the SQL statement
-    cursor.execute('SELECT * FROM jobs;')
+    cursor.execute(
+        '''SELECT j.job_id, j.job_name, j.company_name, j.location, JSON_ARRAYAGG(k.skill_name)
+        FROM jobs j JOIN jobskills s ON j.job_id = s.job_id 
+        JOIN skills k ON s.skill_id = k.skill_id
+        GROUP BY job_id;'''
+    )
     result = cursor.fetchall()
 
+    jobInfo = result
 
-    for i in result:
-        # print(i.keys()) #Gives the different column names (the keys) from the result that you get from cursor.fetchall()
-        print("{ jobTitle: \"" + str(i['job_name']) + "\", company: \"" + str(i['company_name']) + "\", location: \"" + str(i['location']) + "\", skills: [\"", end="") #EXAMPLE: Returns the data i job_name, company_name, location
-        #Fetch jobs for the job id:
-        cursor.execute('SELECT i.job_id, s.skill_name FROM jobs i, jobskills j, skills s WHERE i.job_id = ' + str(i['job_id']) + ' AND i.job_id = j.job_id AND j.skill_id = s.skill_id;')
-        skills = cursor.fetchall()
-        for j in skills:
-            print(str(j['skill_name']) + "\", \"", end="")
-        print("] },\n")
+
+    # for i in result:
+    #     # print(i.keys()) #Gives the different column names (the keys) from the result that you get from cursor.fetchall()
+    #     print("{ jobTitle: \"" + str(i['job_name']) + "\", company: \"" + str(i['company_name']) + "\", location: \"" + str(i['location']) + "\", skills: [\"", end="") #EXAMPLE: Returns the data i job_name, company_name, location
+    #     #Fetch jobs for the job id:
+    #     cursor.execute('SELECT i.job_id, s.skill_name FROM jobs i, jobskills j, skills s WHERE i.job_id = ' + str(i['job_id']) + ' AND i.job_id = j.job_id AND j.skill_id = s.skill_id;')
+    #     skills = cursor.fetchall()
+    #     for j in skills:
+    #         print(str(j['skill_name']) + "\", \"", end="")
+    #     print("] },\n")
 
     #Above is the complete MySQL data to give to the scirpt in HTML
 
